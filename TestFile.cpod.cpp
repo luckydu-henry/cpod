15b7c9b8a5027c6aa84e0005f083f5d1c7ec5eed
luckydu-henry <13552325266@163.com>
2025-06-14 12:45:11 +0800

    Fix ; inside string bug and add pair/tuple code interface.

--- TestFile.cpod.cpp
+++ TestFile.cpod.cpp
@@ -2,6 +2,7 @@
 #include <string>  // For string.
 #include <utility>
 using namespace std;
+
 /* ***********************************************************************
 * Test cpod file
 * This file shows how you can use cpod to (de)serialize basic types and
@@ -11,16 +12,18 @@ using namespace std;
 
 pair<string, float> p = {"Hello world!", 1.234F};
 
-string myName          = "Henry Du";
-int    myAge           = 17;
-bool   amIaBoy         = true;
-float  myHeight        = 169.9F;
-float  myWidth         = 70.456F; // in kg.
-string myEmails[4]     = { 
+string  myName          = "Henry Du";
+int     myAge           = 17;
+bool    amIaBoy         = true;
+float   myHeight        = 169.9F;
+float   myWidth         = 70.456F; // in kg.
+string  myEmails[6]     = { 
     "wotsukoroga94@gmail.com", 
     "dududu_721@qq.com", 
     "13552325266@163.com",
-    "18516915799@126.com"
+    "18516915799@126.com",
+    "xidhyu@outlook.com",
+    "I do have plenty of emails!"
 };
 
 string shaderCode = 
